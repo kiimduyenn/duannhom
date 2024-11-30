dbf9a30b5bf1d891d636fbbb7aaef71a74c6ecca
kiimduyenn <kimduyen1532004@gmail.com>
2024-11-30 17:08:32 +0700

    kd bỏ lên admin

--- DIVA/admin.py
+++ DIVA/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from DIVA.models import (DichVu, NguoiDung, Profile, LichHen, KhieuNai_LichHen,
+                         KhieuNai_DichVu, YeuCauTuVan, DichVuDaDung, YeuCau_DichVu, LichHen_DichVu)
+
 
 # Register your models here.
+admin.site.register(DichVu)
+admin.site.register(NguoiDung)
+admin.site.register(Profile)
+admin.site.register(LichHen)
+admin.site.register(KhieuNai_DichVu)
+admin.site.register(KhieuNai_LichHen)
+admin.site.register(YeuCauTuVan)
+admin.site.register(DichVuDaDung)
+admin.site.register(YeuCau_DichVu)
+admin.site.register(LichHen_DichVu)
+
