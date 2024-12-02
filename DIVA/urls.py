43fad60ef47f301f511d5b551473a51a6d4da1b5
khanhhuyentmdt <khanhhuyen17112004@gmail.com>
2024-12-02 21:20:09 +0700

    KH tai len

--- DIVA/urls.py
+++ DIVA/urls.py
@@ -17,8 +17,7 @@ urlpatterns = [
     path('cus/',viewsnhi.customer, name="customer"),
     path('ad/',viewsnhi.admin, name="ad"),
     path('emp/',viewsnhi.employee, name="emp"),
-
-
+    path('ds-khach-hang/', viewshuyen.dskhachhang, name='ds_khach_hang'),
     path('thong-tin-khach-hang/<str:username>/', viewshuyen.xemthongtinkhachhang, name='thong_tin_khach_hang'),
     path('khach-hang/', viewshuyen.sua_themthongtinkhachhang, name='them_khach_hang'),
     path('khach-hang/<str:username>/', viewshuyen.sua_themthongtinkhachhang, name='sua_khach_hang'),
