fae0a85e603bc9c15703b80e00ec269c37b0cd76
Quynhnhi2oo7 <zoigoalkeeper@gmail.com>
2024-12-02 08:55:52 +0700

    qnhi di ngu luc 4h30

--- DIVA/urls.py
+++ DIVA/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('them/', viewsngoc.them_dich_vu, name='them_dich_vu'),
     path('capnhat/<str:MaDV>/', viewsngoc.cap_nhat_dich_vu, name='cap_nhat_dich_vu'),
     path('xoa/<str:MaDV>/', viewsngoc.xoa_dich_vu, name='xoa_dich_vu'),
-    path('cus',viewsnhi.customer, name="customer"),
-    path('ad',viewsnhi.admin, name="ad")
+    path('cus/',viewsnhi.customer, name="customer"),
+    path('ad/',viewsnhi.admin, name="ad")
 ]
 
