0754f62ec83414b4494242edbdfdce26c2501c32
kiimduyenn <kimduyen1532004@gmail.com>
2024-12-03 00:16:01 +0700

    kd

--- DIVA/views/viewshuyen.py
+++ DIVA/views/viewshuyen.py
@@ -10,14 +10,14 @@ from django.contrib.auth.decorators import user_passes_test
 def is_staff_or_admin(user):
     return user.is_staff or user.is_superuser
 
-@user_passes_test(is_staff_or_admin)
+# @user_passes_test(is_staff_or_admin)
 def dskhachhang(request):
     # Lấy thông tin từ Profile liên kết với NguoiDung có VaiTro là "Khách hàng"
-    khachhang = Profile.objects.filter(vaitro='Khách hàng', is_Enable=True)
-    return render(request, 'dskhachhang.html', {'khachhang': khachhang})
+    profile = Profile.objects.filter(vaitro='Khách hàng', is_Enable=True)
+    return render(request, 'dskhachhang.html', {'profile': profile})
 
 
-@user_passes_test(is_staff_or_admin)
+# @user_passes_test(is_staff_or_admin)
 def xemthongtinkhachhang(request, username):
     try:
         # Lấy thông tin người dùng và profile của họ
@@ -35,16 +35,16 @@ def xemthongtinkhachhang(request, username):
         messages.error(request, "Profile không tồn tại")
         return HttpResponse("Profile không tồn tại", status=404)
 
-@user_passes_test(is_staff_or_admin)
+# @user_passes_test(is_staff_or_admin)
 def sua_themthongtinkhachhang(request, username=None):
-    # Nếu `id` được truyền, lấy thông tin khách hàng hiện có (sửa)
+    # Nếu id được truyền, lấy thông tin khách hàng hiện có (sửa)
     if username:
         user = get_object_or_404(User, username=username)
         khach_hang = get_object_or_404(Profile, MaUser=user)
         form = KhachHangForm(instance=khach_hang)
         action = "Cập Nhật"
     else:
-        # Nếu không có `id`, tạo mới (thêm)
+        # Nếu không có id, tạo mới (thêm)
         khach_hang = None
         form = KhachHangForm()
         action = "Thêm"
@@ -61,9 +61,15 @@ def sua_themthongtinkhachhang(request, username=None):
 
     return render(request, 'themkhachhang.html', {'form': form, 'action': action})
 
-@user_passes_test(is_staff_or_admin)
+# @user_passes_test(is_staff_or_admin)
 def xoakhachhang(request):
-    profile = get_object_or_404(Profile, user=request.user)
-    profile.delete()
-    messages.success(request, f"Thông tin khách hàng {profile.hoten} đã được xóa thành công!")
-    return redirect('ds_khach_hang')
+    if request.method == 'POST':
+        username = request.POST.get('MaUser')  # Lấy giá trị từ form
+        try:
+            user = User.objects.get(username=username)
+            profile = get_object_or_404(Profile, MaUser=user)  # Hoặc thay MaUser bằng trường khác nếu cần
+            profile.delete()
+            messages.success(request, f"Thông tin khách hàng {profile.hoten} đã được xóa thành công!")
+        except ValueError:
+            messages.error(request, "Dữ liệu không hợp lệ. Vui lòng thử lại.")
+        return redirect('ds_khach_hang')
